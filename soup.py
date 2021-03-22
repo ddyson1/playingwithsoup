c5bdbab0ccde046917e9c25badac287f859f2ea1
Devin Dyson <dyson@bu.edu>
2021-03-22 10:27:36 -0400

    added gTTS to read headlines with UK accent

--- soup.py
+++ soup.py
@@ -1,12 +1,13 @@
+from bs4 import BeautifulSoup
 import requests
+from gtts import gTTS
+import os
 
 page = requests.get("https://english.elpais.com/")
 # http://127.0.0.1:8000/screen/add_stock.html
 
 # print(page.status_code)
 # print(page.content)
-
-from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
@@ -17,6 +18,10 @@ coverpage = soup.find_all('h2')
 for i in range(len(soup.find_all('h2'))):
     print(i + 1)
     print(coverpage[i].get_text())
+    headlines = coverpage[i].get_text()
+    speech = gTTS(text = headlines, lang = "en", tld = "co.uk", slow = False)
+    speech.save("headlines.mp3")
+    os.system("afplay headlines.mp3")
 
 # soup.find_all('p')[0].get_text()
 # print(soup.prettify())
