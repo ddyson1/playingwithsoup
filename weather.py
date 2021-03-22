355a627486e43494cd2e36f638724ed6dea103cb
Devin Dyson <dyson@bu.edu>
2021-03-21 23:11:50 -0400

    added user input

--- weather.py
+++ weather.py
@@ -1,7 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-page = requests.get("https://forecast.weather.gov/MapClick.php?lat=42.3587&lon=-71.0567#.YFfPbGRKhro")
+cities = ["Boston", "NYC", "Weston"]
+print(cities)
+
+city = input("Pick a city from the array above to hear the weekly forecast for that city: ")
+
+if city == cities[0]:
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=42.3587&lon=-71.0567#.YFfPbGRKhro")
+elif city == cities[1]:
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=40.7146&lon=-74.0071#.YFgJXmRKhro")
+elif city == cities[2]:
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=41.2244&lon=-73.383")
+else:
+    print("The city you entered is not on the list. Defaulting to Boston, MA")
+    page = requests.get("https://forecast.weather.gov/MapClick.php?lat=42.3587&lon=-71.0567#.YFfPbGRKhro")
+
+# page = requests.get("https://forecast.weather.gov/MapClick.php?lat=42.3587&lon=-71.0567#.YFfPbGRKhro")
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
