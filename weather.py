b4a49199cd8c69a1219a6f223d078f91b51b1d69
Devin Dyson <dyson@bu.edu>
2021-03-21 22:59:45 -0400

    iterates through weekly forecast

--- weather.py
+++ weather.py
@@ -5,21 +5,36 @@ page = requests.get("https://forecast.weather.gov/MapClick.php?lat=42.3587&lon=-
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
-sevenday = soup.find(id="seven-day-forecast")
-forecast = sevenday.find_all(class_="tombstone-container")
+# sevenday = soup.find(id="seven-day-forecast")
+# forecast = sevenday.find_all(class_="tombstone-container")
 
-tonight = forecast[0]
+detailed = soup.find(id="detailed-forecast")
+weekly = detailed.find_all(class_="forecast-label")
+weather = detailed.find_all(class_="forecast-text")
 
-img = tonight.find("img")
-desc = img['title']
+# tonight = forecast[0]
+# monday = forecast[1]
 
-print(desc)
+# img = tonight.find("img")
+# desc = img['title']
+
+# print(desc)
 
 from gtts import gTTS
 import os
 
-speech = gTTS(text = desc, slow = False)
+"""
+for i in range(len(forecast)):
+    print(forecast[i].find("img")['title'])
+    desc = forecast[i].find("img")['title']
+    speech = gTTS(text = desc, slow = False)
+    speech.save("desc.mp3")
+    os.system("afplay desc.mp3")
+"""
 
-speech.save("desc.mp3")
-
-os.system("afplay desc.mp3")
+for i in range(len(weekly)):
+    print(weekly[i].string + "." + " " + weather[i].string)
+    desc = weekly[i].string + "." + " " + weather[i].string
+    speech = gTTS(text = desc, slow = False)
+    speech.save("desc.mp3")
+    os.system("afplay desc.mp3")
